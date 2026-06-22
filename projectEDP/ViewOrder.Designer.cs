c8a0ba07d991c8db04a42b9ac22f2a23f5e1fa12
amsyar7806 <m.amsyar7806@gmail.com>
2026-06-23 04:16:11 +0800

    order

---
+++ projectEDP/ViewOrder.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace projectEDP
+{
+    partial class ViewOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            btnDone = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(246, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 32);
+            label1.TabIndex = 0;
+            label1.Text = "View Order";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(26, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 17);
+            label2.TabIndex = 1;
+            label2.Text = "List Order";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(26, 127);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(618, 454);
+            dataGridView1.TabIndex = 2;
+            // 
+            // btnDone
+            // 
+            btnDone.BackColor = SystemColors.ButtonHighlight;
+            btnDone.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDone.ForeColor = SystemColors.ActiveCaptionText;
+            btnDone.Location = new Point(514, 641);
+            btnDone.Name = "btnDone";
+            btnDone.Size = new Size(130, 45);
+            btnDone.TabIndex = 4;
+            btnDone.Text = "Done";
+            btnDone.UseVisualStyleBackColor = false;
+            // 
+            // ViewOrder
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MidnightBlue;
+            ClientSize = new Size(682, 698);
+            Controls.Add(btnDone);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
+            Name = "ViewOrder";
+            Text = "ViewOrder";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Button btnDone;
+    }
+}
